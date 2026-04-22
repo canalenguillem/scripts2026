0d6c98c09bedf91f8a445052a47f1eeb65478443
enguillem <canalenguillem@gmail.com>
2026-04-22 15:39:33 +0200

    PARELLS SENARS

--- 04_parells.sh
+++ 04_parells.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+#CONTROL D'ERROR
+if [ $# -eq 0 ]; then
+    echo "has de posar al manco un paràmetre"
+    exit 1
+fi
 
-MODUL=$(($1 % 2))
+PARELLS=0
+IMPARELLS=0
 
-echo "modul val $MODUL"
-PARES=0
-IMPARES=0
+while [ ${#} -gt 0 ]; do
+    if [ $((${1} % 2 )) -eq 0 ]; then
+        PARELLS=$((PARELLS+1))
+    else
+        IMPARELLS=$((IMPARELLS+1))
+    fi
+    shift
+done
+
+echo "Hi ha  ${PARELLS} números parells"
+echo "Hi ha  ${IMPARELLS} números imparells"
